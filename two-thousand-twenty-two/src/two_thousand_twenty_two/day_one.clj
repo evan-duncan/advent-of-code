fbe8ee78c6acc8f348f9f8d35aa9afbccddce862
Evan Duncan <evan.duncan@greenhouse.io>
2022-12-01 08:52:01 -0600

    Day one part two

--- two-thousand-twenty-two/src/two_thousand_twenty_two/day_one.clj
+++ two-thousand-twenty-two/src/two_thousand_twenty_two/day_one.clj
@@ -4,11 +4,21 @@
 
 (def puzzle-input (read-resource "day_one.txt"))
 
+(defn calculate-calories-per-elf [input]
+  (->> (split puzzle-input #"\n{2}")
+       (map #(split-lines %))
+       (map #(apply list %))
+       (map #(map (fn [s] (Integer/parseInt s)) %))
+       (map #(apply + %))))
+
 ;; Part one
 ;; Find the elf with the highest number of calories
-(->> (split puzzle-input #"\n{2}")
-     (map #(split-lines %))
-     (map #(apply list %))
-     (map #(map (fn [s] (Integer/parseInt s)) %))
-     (map #(apply + %))
+(->> (calculate-calories-per-elf puzzle-input)
      (apply max))
+
+;; Part two
+;; Find the top three elves
+(->> (calculate-calories-per-elf puzzle-input)
+     (sort >)
+     (take 3)
+     (apply +))
