e137bee62b6dc9cb0a33de3d217a0b5d608af0f9
Evan Duncan <evan.duncan@greenhouse.io>
2022-12-02 23:49:59 -0600

    Day 3 Part 2

--- two-thousand-twenty-two/src/two_thousand_twenty_two/day_three.clj
+++ two-thousand-twenty-two/src/two_thousand_twenty_two/day_three.clj
@@ -1,6 +1,6 @@
 (ns two-thousand-twenty-two.day-three
   (:require [two-thousand-twenty-two.core :refer [read-resource]]
-            [clojure.string :refer [split-lines]]
+            [clojure.string :refer [split-lines split]]
             [clojure.set :refer [intersection]]))
 
 (def puzzle-input (read-resource "day_three.txt"))
@@ -14,6 +14,16 @@
     (merge large small)))
 
 ;; part 1
-(let [rucksacks (->> puzzle-input split-lines (map seq) (map #(partition (/ (count %) 2) %)))
+(let [rucksacks (->> puzzle-input split-lines (map #(partition (/ (count %) 2) %)))
       duplicates (map (fn [[c1 c2]] (intersection (set c1) (set c2))) rucksacks)]
   (->> duplicates (map seq) flatten (map points) (apply +)))
+
+;; part 2
+(->> puzzle-input
+     split-lines
+     (partition 3)
+     (map (fn [[ruck1 ruck2 ruck3]] (intersection (set ruck1) (set ruck2) (set ruck3))))
+     (map seq)
+     flatten
+     (map points)
+     (apply +))
